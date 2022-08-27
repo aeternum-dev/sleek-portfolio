9f67fd8ba3b92d1e328b1c0349d86970c4f76fe8
aeternum-dev <viccesvok4@gmail.com>
2022-08-27 14:02:10 +0200

    Delete unneeded pages and all their subcomponents

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
 import Education from "../pages/education/EducationComponent";
-import Experience from "../pages/experience/Experience";
-import Opensource from "../pages/opensource/Opensource";
 import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
 
@@ -23,22 +21,9 @@ export default class Main extends Component {
               render={(props) => <Home {...props} theme={this.props.theme} />}
             />
             <Route
-              path="/experience"
-              exact
-              render={(props) => (
-                <Experience {...props} theme={this.props.theme} />
-              )}
-            />
-            <Route
               path="/education"
               render={(props) => (
                 <Education {...props} theme={this.props.theme} />
-              )}
-            />
-            <Route
-              path="/opensource"
-              render={(props) => (
-                <Opensource {...props} theme={this.props.theme} />
               )}
             />
             <Route
